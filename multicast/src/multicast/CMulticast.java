5525415752cadd0a6de3f223505f23da292c908c
ItzayanaPH <itzayana.ipn.escom@hotmail.com>
2016-09-11 00:37:49 -0500

    Edicion del Cliente
    Agregue datagrama para conexion de otros clientes

--- multicast/src/multicast/CMulticast.java
+++ multicast/src/multicast/CMulticast.java
@@ -11,9 +11,14 @@ public class CMulticast {
             MulticastSocket cl = new MulticastSocket(9999);
             System.out.println("Cliente escuchando puerto "+cl.getLocalPort());
             cl.setReuseAddress(true);
+            //Lo que tiene  el Servidor
+            cl.setTimeToLive(1);
+            String msjC = "Hola Cliente";
+            byte[] b = msjC.getBytes();
+            //
             try{
                 gpo = InetAddress.getByName("228.1.1.1");
-                
+
             }catch(UnknownHostException u){
                 System.err.println("Dirección no Valida");
             }
@@ -25,6 +30,15 @@ public class CMulticast {
                 String msj = new String(p.getData());
                 System.out.println("Datagrama recibido ... "+msj);
                 System.out.println("Servidor descubierto: "+p.getAddress()+" puerto: "+p.getPort());
+
+                DatagramPacket pC = new DatagramPacket(b,b.length,gpo,9999);
+                cl.send(pC);
+                System.out.println("Enviando mensaje desde el cliente: "+ msjC + "con un TTL = "+ cl.getTimeToLive());
+                try{
+                  Thread.sleep(3000);
+                } catch (InterruptedException ie){
+                  ie.printStackTrace();
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
--- multicast/src/multicast/SMulticast.java
+++ multicast/src/multicast/SMulticast.java
@@ -30,5 +30,4 @@ public class SMulticast {
             e.printStackTrace();
         }
     }
-
 }
